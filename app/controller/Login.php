572d1d4049cf24d00d299f2c8b2bbbbeb4808508
natan-matos-souza <natanmatos_18@hotmail.com>
2023-11-01 18:40:01 -0300

    verifying user credentials

--- app/controller/Login.php
+++ app/controller/Login.php
@@ -6,26 +6,39 @@ use app\model\Admin;
 use app\services\FlashMessage;
 use app\view\View;
 
+use Slim\Routing\RouteContext;
+
 class Login extends View
 {
     public function index($request, $response)
     {
         $this->setView('login.html');
 
-        $this->getView()->render($response, self::$viewName);
+        $this->getView()->render($response, self::$viewName, [
+            "hasFlashMessage" => FlashMessage::hasFlashMessage(),
+            "flashMessageType" => FlashMessage::showFlashMessageType(),
+            "flashMessageText" => FlashMessage::showFlashMessage()
+        ]);
+
+        FlashMessage::destroy();
 
         return $response;
     }
 
-    public function auth($request, $response)
+    public function auth($request, $response, $args)
     {
-
-       $data = $request->getParsedBody();
-
-
-       var_dump($data);
         
-       return $response;
+        FlashMessage::createErrorMessage('Dados inválidos!');
+
+
+
+
+
+
+
+        return $response
+        ->withHeader('Location', 'http://localhost:8082/login')
+        ->withStatus(302);
 
     }
 }
--- app/model/Admin.php
+++ app/model/Admin.php
@@ -13,4 +13,38 @@ class Admin extends Model
     {
 
     }
+
+    public static function isValid(object $data)
+    {
+        $isValid = false;
+        
+
+        $findUser = self::database()->query("SELECT password from admins WHERE email=$data->email");
+
+        if ($findUser->num_rows < 1)
+        {
+            return $isValid = false;
+        }
+
+        $userPassword = $findUser->fetch_all(MYSQLI_ASSOC);
+
+        if (password_verify($data->password, $userPassword))
+        {
+            
+            $userInfo = (object) self::database()->query("SELECT * from admins WHERE email=$email")->fetch_all(MYSQLI_ASSOC);
+
+
+
+            $_SESSION['admin'] = true;
+            $_SESSION['adminUsername'] = $userInfo->username;
+            
+
+
+        } else {
+            return $isValid = false;
+        }
+
+    }
+
+
 }
